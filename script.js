33a0461c8c037ea60a19299fb0c102281e800367
Luke Keltner <lkeltner87@gmail.com>
2017-07-27 10:50:13 -0400

    2nd Commit

--- script.js
+++ script.js
@@ -1,51 +1,143 @@
-function write(id, content)
+
+function write(id, content, join)
 {
+	if (join)
+	{
+		content = content.join(" ");
+	}
+
 	document.getElementById(id).innerHTML=content;
 }
 
-
-var words = ['hello', 'apple', 'mouse', 'coffee', 'lantern', 'mug', 'sausage', '98638643862345879'];
-
-var wordToGuess = Math.floor(Math.random() * words.length);
-
-write('test', words[wordToGuess]);
-
-var letters = words[wordToGuess].split("");
-  
-document.getElementById("split").innerHTML = letters;
-
-var empty = [];
-
-for (var i=0; i<letters.length; i++)
+function colorOfAlreadyGuessed(color)
 {
-	empty.push('____');
+	var getText = document.getElementById("alreadyGuessed").innerHTML;
+	var changeColor = getText.fontcolor(color);
+	write('alreadyGuessed', changeColor, false)
 }
 
+function resetGame()
+{
+	//Setting up word bank and picking one at random.
+	var words = ['hello', 'apple', 'mouse', 'coffee', 'lantern', 'mug', 'sausage'];
+	var index = Math.floor(Math.random() * words.length);
+	var wordToGuess = words[index];
+	write('test', wordToGuess, false); //Showing the word the computer picked for debugging purposes.
 
-empty = empty.join('	');
+	//Splitting up the word into its individual letters - letters is an array of letters.
+	//We're also setting up a boolean correctLettersGuessed to keep track of when/if the users wins.
+	//The user wins when correctLettersGuessed = letter.length.
+	var correctLettersGuessed = 0;
+	var letters = wordToGuess.split("");
 
-document.getElementById("empty").innerHTML = empty;
-times = 0;
+	//Defining the Current Word array, intially set to all blank letters. This is the array that will change as the user makes guesses.
+	var currentWord = [];
+	for (var i=0; i<letters.length; i++)
+	{
+		currentWord.push('____');
+	}
+	write('currentWord', currentWord, true)
 
+	//Declaring variables and setting their intial values.
+	var guesses = 12;
+	write('guesses', guesses, false);
+	var guessedLetters = []
+}
+
+//Setting up word bank and picking one at random.
+var words = ['hello', 'apple', 'mouse', 'coffee', 'lantern', 'mug', 'sausage'];
+var index = Math.floor(Math.random() * words.length);
+var wordToGuess = words[index];
+write('test', wordToGuess, false); //Showing the word the computer picked for debugging purposes.
+
+//Splitting up the word into its individual letters - letters is an array of letters.
+//We're also setting up a boolean correctLettersGuessed to keep track of when/if the users wins.
+//The user wins when correctLettersGuessed = letter.length.
+var correctLettersGuessed = 0;
+var letters = wordToGuess.split("");
+
+//Defining the Current Word array, intially set to all blank letters. This is the array that will change as the user makes guesses.
+var currentWord = [];
+for (var i=0; i<letters.length; i++)
+{
+	currentWord.push('____');
+}
+write('currentWord', currentWord, true)
+
+//Declaring variables and setting their intial values.
+var guesses = 12;
+var wins = 0;
+write('guesses', guesses, false);
+write('wins', wins, false);
+
+//Declaring an array to keep track of what letters the user has guessed.
+var guessedLetters = []
+
+//The events that occur when the User presses a key.
 document.onkeyup = function(event)
 {
-	for (var i=0; i<letters.length; i++)	
- 	{
-		document.getElementById("pressed").innerHTML = event.key
-		document.getElementById("checking").innerHTML = letters[i]
+	//Setting the pressed key to lowercase just for consistency.
+	var guessedLetter = event.key.toLowerCase();
 
-		if (event.key == letters[i])
-		{
-			//document.getElementById("empty").innerHTML = 'You pressed the right letter!';
-			document.getElementById("empty").innerHTML = 'empty is '+empty;
-			times = times + 1;
-		}
+	//We assume that the users guessed letter has not already been guessed
+	alreadyGuessed = false;
 
-		else
+	//This loop goes through all letters that have been already guessed.
+	for (var i=0; i<guessedLetters.length; i++)
+	{
+		//If the user's letter has already been guessed, we set the alreadyGuessed Boolean to true.
+		if (guessedLetter === guessedLetters[i])
 		{
-			document.getElementById("empty").innerHTML = 'noooooooo!'
+			alreadyGuessed = true;
+			break;
 		}
 	}
 
-	write('times', times);
+
+	//If the previous loop did not set alreadyGuessed to true, that means the user has yet to guess it 
+	//so we now put it in the guessedLetters array and also print it to the document as a guessed letter
+	if(alreadyGuessed == false)
+	{
+		guessedLetters.push(guessedLetter);
+		var userGuesses = document.getElementById("userGuesses").innerHTML;
+		var updatedUserGuesses = userGuesses.concat(' '+guessedLetter);
+		write('userGuesses', updatedUserGuesses, false);
+	}
+
+	//We now assume that the letter the user guessed is an incorrect letter
+	var correctGuess = false;
+
+	//This loop looks through all letters of the word that is to be guessed.
+	for (var i=0; i<letters.length; i++)	
+ 	{
+
+ 		//If the user guessed a letter correctly, we set the boolean correctGuess to true and replace the blank space in currentWord
+ 		//with the letter that was guessed.  This loop will go through all letters in the word so it will print duplicates if
+ 		//there are any.  
+		if (guessedLetter == letters[i])
+		{
+			correctGuess = true;
+			currentWord.splice(i, 1, guessedLetter);
+			write('currentWord', currentWord, true);
+			correctLettersGuessed = correctLettersGuessed + 1
+		}
+	}
+
+	//If the user guessed incorrectly...
+	if (correctGuess==false)
+	{
+		//...and also has not guessed this letter before, we subtract one from the guess total and write to the document.
+		if (alreadyGuessed==false)
+		{
+			guesses = guesses - 1;
+			write('guesses', guesses, false);
+		}
+	}
+
+	if (correctLettersGuessed == letters.length)
+	{
+		wins = wins + 1;
+		write('wins', wins, false);
+		resetGame();
+	}
 }
